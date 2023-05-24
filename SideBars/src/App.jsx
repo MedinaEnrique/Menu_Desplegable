c656c6eb95fc0f24a6390d7854fbb3b7065d8a4f
MedinaEnrique <133240252+MedinaEnrique@users.noreply.github.com>
2023-05-24 17:35:22 -0400

    falta el card

--- SideBars/src/App.jsx
+++ SideBars/src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Container from "./assets/components/Container";
+import Card from "./assets/components/Card";
 
 function App() {
   
 
   return (
-    <Container/>
-    
+    <>
+      <Container />
+      <Card />
+    </>
   );
 }
 
