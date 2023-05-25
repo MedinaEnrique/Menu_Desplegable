5d225f72e84bad53fdbf4e537b150930fdb47621
MedinaEnrique <133240252+MedinaEnrique@users.noreply.github.com>
2023-05-25 16:00:21 -0400

    3er ejercicio en proceso

---
+++ SideBars/src/assets/components/AccordionContainer.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const AccordionContainer =()=> {
+    const dataA = [
+        {
+          header: "HoldBack",
+          content:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima, assumenda quo tempora ducimus a impedit eum laboriosam? Sit autem ipsum earum molestiae, reiciendis consequuntur et dolorem totam laboriosam doloremque ab?",
+        },
+        {
+          header: "Financial Assurance",
+          content:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima, assumenda quo tempora ducimus a impedit eum laboriosam? Sit autem ipsum earum molestiae, reiciendis consequuntur et dolorem totam laboriosam doloremque ab?",
+        },
+        {
+          header: "Insurance Plans",
+          content:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima, assumenda quo tempora ducimus a impedit eum laboriosam? Sit autem ipsum earum molestiae, reiciendis consequuntur et dolorem totam laboriosam doloremque ab?",
+        },
+        {
+          header: "ARO creditor",
+          content:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima, assumenda quo tempora ducimus a impedit eum laboriosam? Sit autem ipsum earum molestiae, reiciendis consequuntur et dolorem totam laboriosam doloremque ab?",
+        },
+      ];
+  return (
+    <>
+    </>
+  )
+}
+
+export default AccordionContainer
